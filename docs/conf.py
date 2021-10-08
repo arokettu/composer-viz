830bd8591c2d7c45e75762bd5e2518ddc176ad87
Anton Smirnov <sandfox@sandfox.me>
2021-10-08 21:26:18 +0300

    Add sphinx config

---
+++ docs/conf.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+project = 'Composer Viz'
+author = 'Anton Smirnov'
+copyright = '{} {}'.format(datetime.now().year, author)
+language = 'en'
+
+html_title = project
+html_theme = 'furo'
+templates_path = ["_templates"]
